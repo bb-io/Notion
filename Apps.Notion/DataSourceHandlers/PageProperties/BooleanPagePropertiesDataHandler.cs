9f0d66aaf5b254ffd0f0bce17433b002a6b55db1
bZverok <137277669+bZverok@users.noreply.github.com>
2023-11-14 11:34:11 +0200

    Fixed data source handlers (#4)
    * removed redundant icon
    
    * finished creation actions
    
    * added comment + improved content actions
    
    * added user/block actions and notion-html parser
    
    * added html converting actions for pages
    
    * Version updated
    
    * added string, number and date page property getters
    
    * added page property setters
    
    * fixed setter inputs
    
    * added database query action
    
    * fixed property data source handlers
    
    ---------
    
    Co-authored-by: Bohdan <127740895+bohdanm-bb@users.noreply.github.com>

--- Apps.Notion/DataSourceHandlers/PageProperties/BooleanPagePropertiesDataHandler.cs
+++ Apps.Notion/DataSourceHandlers/PageProperties/BooleanPagePropertiesDataHandler.cs
@@ -1,5 +1,6 @@
 using Apps.Notion.DataSourceHandlers.PageProperties.Base;
 using Apps.Notion.Models.Request.Page.Properties.Getter;
+using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Invocation;
 
 namespace Apps.Notion.DataSourceHandlers.PageProperties;
@@ -8,7 +9,7 @@ public class BooleanPagePropertiesDataHandler : PagePropertiesDataHandler
 {
     protected override string[] Types => new[] { "checkbox" };
 
-    public BooleanPagePropertiesDataHandler(InvocationContext invocationContext, PageBooleanPropertyRequest input) : base(
+    public BooleanPagePropertiesDataHandler(InvocationContext invocationContext, [ActionParameter] PageBooleanPropertyRequest input) : base(
         invocationContext, input.PageId)
     {
     }
