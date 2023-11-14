9b1c64afec92f21414a30e21f90589a7a160ab85
bzveriok <bohdan.zverok@blackbird.io>
2023-11-14 11:32:52 +0200

    fixed property data source handlers

--- Apps.Notion/DataSourceHandlers/PageProperties/FilesPagePropertiesDataHandler.cs
+++ Apps.Notion/DataSourceHandlers/PageProperties/FilesPagePropertiesDataHandler.cs
@@ -1,5 +1,6 @@
 using Apps.Notion.DataSourceHandlers.PageProperties.Base;
 using Apps.Notion.Models.Request.Page.Properties.Getter;
+using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Invocation;
 
 namespace Apps.Notion.DataSourceHandlers.PageProperties;
@@ -8,7 +9,7 @@ public class FilesPagePropertiesDataHandler : PagePropertiesDataHandler
 {
     protected override string[] Types => new[] { "files" };
 
-    public FilesPagePropertiesDataHandler(InvocationContext invocationContext, PageFilesPropertyRequest input) : base(
+    public FilesPagePropertiesDataHandler(InvocationContext invocationContext, [ActionParameter] PageFilesPropertyRequest input) : base(
         invocationContext,
         input.PageId)
     {
