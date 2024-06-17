3f31c0a20fd25df9db7df8d7bd617366d9f12ee5
bZverok <137277669+bZverok@users.noreply.github.com>
2024-06-17 16:11:18 +0300

    Added polling events (#10)
    * added polling events
    
    * polling updates
    
    * Update Apps.NotionOAuth.csproj

---
+++ Apps.NotionOAuth/PollingEvents/PollingList.cs
@@ -0,0 +1,69 @@
+using Apps.NotionOAuth.Invocables;
+using Apps.NotionOAuth.Models.Entities;
+using Apps.NotionOAuth.Models.Response.Page;
+using Apps.NotionOAuth.PollingEvents.Models.Memory;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common.Polling;
+
+namespace Apps.NotionOAuth.PollingEvents;
+
+[PollingEventList]
+public class PollingList : NotionInvocable
+{
+    public PollingList(InvocationContext invocationContext) : base(invocationContext)
+    {
+    }
+
+    [PollingEvent("On pages created", "On new pages created")]
+    public Task<PollingEventResponse<DateMemory, ListPagesResponse>> OnPagesCreated(
+        PollingEventRequest<DateMemory> request)
+        => HandlePagesPolling(request,
+            x => x.CreatedTime > request.Memory?.LastInteractionDate);
+
+    [PollingEvent("On pages updated", "On any pages updated")]
+    public Task<PollingEventResponse<DateMemory, ListPagesResponse>> OnPagesUpdated(
+        PollingEventRequest<DateMemory> request)
+        => HandlePagesPolling(request,
+            x => x.LastEditedTime > request.Memory?.LastInteractionDate);
+
+    private async Task<PollingEventResponse<DateMemory, ListPagesResponse>> HandlePagesPolling(
+        PollingEventRequest<DateMemory> request, Func<PageResponse, bool> filter)
+    {
+        if (request.Memory == null)
+        {
+            return new()
+            {
+                FlyBird = false,
+                Memory = new()
+                {
+                    LastInteractionDate = DateTime.UtcNow
+                }
+            };
+        }
+
+        var items =
+            (await Client.SearchAll<PageResponse>(Creds, "page"))
+            .Where(filter)
+            .ToArray();
+
+        if (items.Length == 0)
+            return new()
+            {
+                FlyBird = false,
+                Memory = new()
+                {
+                    LastInteractionDate = DateTime.UtcNow
+                }
+            };
+
+        return new()
+        {
+            FlyBird = true,
+            Memory = new()
+            {
+                LastInteractionDate = DateTime.UtcNow
+            },
+            Result = new(items.Select(x => new PageEntity(x)).ToArray())
+        };
+    }
+}
