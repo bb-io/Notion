5a5ab8462160a71f1717d3dd9ac2f648d4a0b6c6
mathijs-bb <mathijs.sonnemans@blackbird.io>
2025-01-24 15:14:05 +0100

    Added on button clicked event

---
+++ Apps.NotionOAuth/CallbackEvents/CallbackEvents.cs
@@ -0,0 +1,30 @@
+﻿using Apps.NotionOAuth.CallbackEvents.Models.Dto;
+using Apps.NotionOAuth.CallbackEvents.Models.Responses;
+using Blackbird.Applications.Sdk.Common.Webhooks;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.NotionOAuth.CallbackEvents;
+
+[WebhookList]
+public class CallbackEvents
+{
+    [Webhook("On button clicked", Description = "Triggered when you click a button on a Notion page. See")]
+    public Task<WebhookResponse<ButtonClickedResponse>> OrderDeleted(WebhookRequest webhookRequest)
+    {
+        var body = JsonConvert.DeserializeObject<ButtonRequest>(webhookRequest.Body.ToString());
+        return Task.FromResult(new WebhookResponse<ButtonClickedResponse>
+        {
+            HttpResponseMessage = new HttpResponseMessage(statusCode: HttpStatusCode.OK),
+            Result = new ButtonClickedResponse()
+            {
+                PageId = body?.Data.Id,
+            }
+        });
+    }
+}
---
+++ Apps.NotionOAuth/CallbackEvents/Models/Dto/ButtonRequest.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.NotionOAuth.CallbackEvents.Models.Dto;
+public class ButtonRequest
+{
+    [JsonProperty("data")]
+    public Data Data { get; set; }
+}
+
+public class Data
+{
+    [JsonProperty("id")]
+    public string Id { get; set; }
+}
