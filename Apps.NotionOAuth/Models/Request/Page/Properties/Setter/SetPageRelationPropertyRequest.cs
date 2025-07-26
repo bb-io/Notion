8826e71d2115b3e0550134b030595f829995afd4
Alex Terehov <terales@users.noreply.github.com>
2025-07-26 19:56:26 -0230

    Improve set relation inputs

--- Apps.NotionOAuth/Models/Request/Page/Properties/Setter/SetPageRelationPropertyRequest.cs
+++ Apps.NotionOAuth/Models/Request/Page/Properties/Setter/SetPageRelationPropertyRequest.cs
@@ -1,10 +1,26 @@
+using Apps.NotionOAuth.DataSourceHandlers;
+using Apps.NotionOAuth.DataSourceHandlers.PageProperties.Getters;
 using Apps.NotionOAuth.Models.Request.Page.Properties.Getter;
 using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.NotionOAuth.Models.Request.Page.Properties.Setter;
 
-public class SetPageRelationPropertyRequest : PageFilesPropertyRequest
+public class SetPageRelationPropertyRequest
 {
-    [Display("Page IDs")]
+    [Display("Database")]
+    [DataSource(typeof(DatabaseDataHandler))]
+    public string DatabaseId { get; set; } = string.Empty;
+
+    [Display("Page")]
+    [DataSource(typeof(PageDataHandler))]
+    public string PageId { get; set; } = string.Empty;
+
+    [Display("Property")]
+    [DataSource(typeof(RelationPagePropertiesDataHandler))]
+    public string PropertyId { get; set; } = string.Empty;
+
+    [Display("Related page IDs")]
+    [DataSource(typeof(PageDataHandler))]
     public IEnumerable<string> RelatedPageIds { get; set; } = [];
 }
