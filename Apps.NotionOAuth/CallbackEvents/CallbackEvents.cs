c64293646784ead057c2dcb65f6e5c567875ee1b
Alex Terehov <terales@users.noreply.github.com>
2025-08-22 12:35:13 -0230

    Add filter to 'On button clicked' event

--- Apps.NotionOAuth/CallbackEvents/CallbackEvents.cs
+++ Apps.NotionOAuth/CallbackEvents/CallbackEvents.cs
@@ -1,13 +1,10 @@
 ﻿using Apps.NotionOAuth.CallbackEvents.Models.Dto;
 using Apps.NotionOAuth.CallbackEvents.Models.Responses;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Exceptions;
 using Blackbird.Applications.Sdk.Common.Webhooks;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Apps.NotionOAuth.CallbackEvents;
 
@@ -15,15 +12,38 @@ namespace Apps.NotionOAuth.CallbackEvents;
 public class CallbackEvents
 {
     [Webhook("On button clicked", Description = "Triggered when you click a button on a Notion page. See")]
-    public Task<WebhookResponse<ButtonClickedResponse>> OrderDeleted(WebhookRequest webhookRequest)
+    public Task<WebhookResponse<ButtonClickedResponse>> OrderDeleted(
+        WebhookRequest webhookRequest,
+        [WebhookParameter, Display("Custom header key")] string filterHeaderName,
+        [WebhookParameter, Display("Custom header contains")] string filterHeaderExpectedPart)
     {
-        var body = JsonConvert.DeserializeObject<ButtonRequest>(webhookRequest.Body.ToString());
+        if (string.IsNullOrWhiteSpace(filterHeaderName) != string.IsNullOrWhiteSpace(filterHeaderExpectedPart))
+            throw new PluginMisconfigurationException("Both filter header name and expected part must be provided or both must be empty.");
+
+        if (string.IsNullOrWhiteSpace(filterHeaderName) == false
+            && webhookRequest.Headers.TryGetValue(filterHeaderName, out var filterHeaderValue)
+            && filterHeaderValue.Contains(filterHeaderExpectedPart, StringComparison.OrdinalIgnoreCase))
+        {
+            return Task.FromResult(new WebhookResponse<ButtonClickedResponse>
+            {
+                ReceivedWebhookRequestType = WebhookRequestType.Preflight, // don't start flight
+                HttpResponseMessage = new HttpResponseMessage(statusCode: HttpStatusCode.OK),
+                Result = null
+            });
+        }
+
+        var body = JsonConvert.DeserializeObject<ButtonRequest>(webhookRequest.Body.ToString() ?? string.Empty)
+            ?? throw new PluginApplicationException($"Can't deserialize button request body. Received: {webhookRequest.Body}");
+
         return Task.FromResult(new WebhookResponse<ButtonClickedResponse>
         {
+            ReceivedWebhookRequestType = WebhookRequestType.Default, // start flight
             HttpResponseMessage = new HttpResponseMessage(statusCode: HttpStatusCode.OK),
             Result = new ButtonClickedResponse()
             {
-                PageId = body?.Data.Id,
+                PageId = body.Data.PageId,
+                ParentType = body.Parent.Type,
+                ParentId = body.Parent.GetParentId(),
             }
         });
     }
