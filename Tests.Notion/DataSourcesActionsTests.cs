8e5a3f9e6f7d8e55b2644db6a5209a6bb4aa78d1
Fesenko-A <111450737+Fesenko-A@users.noreply.github.com>
2026-09-16 08:47:07 +0200

    add OptionalViewRequest to SearchPagesInDatasource

--- Tests.Notion/DataSourcesActionsTests.cs
+++ Tests.Notion/DataSourcesActionsTests.cs
@@ -1,6 +1,7 @@
 ﻿using Apps.NotionOAuth.Actions;
 using Apps.NotionOAuth.Models.Request.DataBase;
 using Apps.NotionOAuth.Models.Request.DataSource;
+using Apps.NotionOAuth.Models.Request.View;
 using Apps.NotionOAuth.Models.Response.Page;
 using Tests.Notion.Base;
 
@@ -16,23 +17,31 @@ public class DataSourcesActionsTests : TestBase
         var action = new DataSourcesActions(InvocationContext);
         var input = new DataSourceRequest
         {
-            DataSourceId = "e5585fd5-8491-4cf1-9e1d-6ddfa93d2761"
+            DataSourceId = "b2e5c99e-5904-4ed4-91f5-fc049d6c60bc"
         };
         var database = new OptionalDatabaseRequest();
+        var searchInput = new SearchPagesInDataSourceRequest();
+        var optionalViewInput = new OptionalViewRequest
+        {
+            ViewId = "244a9644-cf02-8031-a30a-000c12539afb"
+        };
 
         // Act
-        var result = await action.SearchPagesInDatasource(input, database, new()
-        {
-        });
+        var result = await action.SearchPagesInDatasource(input, database, searchInput, optionalViewInput);
 
         // Assert
         Assert.IsNotNull(result);
         Assert.IsInstanceOfType(result, typeof(ListPagesResponse));
         Assert.IsTrue(result.Items.Count > 0, "Expected at least one page matching the filter.");
+        Console.WriteLine($"Total items: {result.Items.Count}");
 
         foreach (var page in result.Items)
         {
-            Console.WriteLine($"Page ID: {page.ContentId}, Created Time: {page.CreatedTime}, Last Edited Time: {page.LastEditedTime}");
+            Console.WriteLine(
+                $"Page ID: {page.ContentId}, " +
+                $"Page Title: {page.Title}, " +
+                $"Created Time: {page.CreatedTime}, " +
+                $"Last Edited Time: {page.LastEditedTime}");
         }
     }
 }
